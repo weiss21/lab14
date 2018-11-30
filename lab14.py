0d7cf9d34db1494c35539a46532556e08fb90ec6
weiss21 <34324418+weiss21@users.noreply.github.com>
2018-11-30 00:32:26 -0800

    Unique count added

--- lab14.py
+++ lab14.py
@@ -1,5 +1,18 @@
 #Lab 14
 
+
+#Problem 1
 def greenEggsHam():
   
+  #Opens Green Eggs and Ham File. Reads it into text form. Then closes file.
   file = open("C:\Users\Weiss\Documents\GitHub\lab14\eggs.txt","rt")
+  fileText = file.read()
+  file.close()
+  
+  #create an array of words. No Spaces or dashes
+  fileText = fileText.lower().replace("-", " ").split()
+  
+  #How many distinct Words.
+  #When this is done. Delete Return, and this comment.
+  unique = set(fileText)
+  return len(unique)
