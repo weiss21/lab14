b8bbf4d12c50e4000f2b7bb48c5254124c69ecf9
weiss21 <34324418+weiss21@users.noreply.github.com>
2018-11-30 17:59:11 -0800

    Step 1 and 2 done. Problem 1

--- lab14.py
+++ lab14.py
@@ -1,18 +1,33 @@
 #Lab 14
 
-
 #Problem 1
 def greenEggsHam():
   
   #Opens Green Eggs and Ham File. Reads it into text form. Then closes file.
   file = open("C:\Users\Weiss\Documents\GitHub\lab14\eggs.txt","rt")
-  fileText = file.read()
+  original = file.read()
   file.close()
   
   #create an array of words. No Spaces or dashes
-  fileText = fileText.lower().replace("-", " ").split()
+  fileText = original.lower().replace("-", " ").split()
   
-  #How many distinct Words.
+  #How many distinct Words. Step 1
   #When this is done. Delete Return, and this comment.
   unique = set(fileText)
-  return len(unique)
+  
+  #Create a count of how often each of the words appears. Step 2 
+  #use a dictionary
+  
+  wordsAppear = {}
+  for i in fileText:
+    wordsAppear[i] = wordsAppear.get(i, 0) + 1
+
+  #reverse the values to be sort.
+  word_freq = []
+  for k, v in wordsAppear.items():
+    word_freq.append((v,k))
+  
+  #Sort from highest to lowest
+  
+  word_freq.sort(reverse = True)
+  return word_freq
