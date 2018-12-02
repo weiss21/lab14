9a610fc52322e6fef487f41aa98f6677507911c7
weiss21 <34324418+weiss21@users.noreply.github.com>
2018-12-01 20:53:42 -0800

    Took an hour to realize '\' is bad but "/ " works

--- lab14_pt2.py
+++ lab14_pt2.py
@@ -3,7 +3,17 @@
 
 #Problem 2
 
+#Step 1. Save HTML file of website on computer completed
+
 def head():
   
-  file = open("C:\Users\Weiss\Documents\GitHub\lab14\foothill.html","rt")
-  original = file.read()
+  #Step 2. Open HTML File
+  file = open("/C:/Users/Weiss/Documents/GitHub/lab14/foot.html", "rt")
+  
+  original = file.read() 
+  file.close()
+  
+
+  #Step 2. Use string processing to extract headline from paper.
+  
+  print len(original)
