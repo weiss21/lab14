597c87903e80f695aed0c9f310d8ab96b0aa7951
weiss21 <34324418+weiss21@users.noreply.github.com>
2018-12-02 16:27:13 -0800

    Problem 2 works now

--- lab14_pt2.py
+++ lab14_pt2.py
@@ -5,15 +5,45 @@
 
 #Step 1. Save HTML file of website on computer completed
 
+#Extra Function
+
 def head():
   
   #Step 2. Open HTML File
-  file = open("/C:/Users/Weiss/Documents/GitHub/lab14/foot.html", "rt")
-  
-  original = file.read() 
+  file = open("/C:/Users/Weiss/Documents/GitHub/lab14/foothill.html", "r")
+  fileText = file.read() 
   file.close()
-  
 
   #Step 2. Use string processing to extract headline from paper.
+  #place Headlines in Array
+  headlines = [] 
+
+  #a = "<h3>"
+  #b = "</h3>"
+  a ="&lt;h3&gt;</span>"
+  b ="<span class=\"html-tag\">&lt;/h3&gt;</span>"
   
-  print len(original)
+  index = 0 
+  start = 0
+  finish = len(fileText)
+  
+  while index != -1:
+    index = fileText.find(a, start, finish)
+    if index != -1:
+      index += len(a)
+    midIndex = fileText.find(b, index, finish)
+    headlines.append(fileText[index:midIndex])
+    start = midIndex
+    
+  for i in headlines:
+    print i
+  return 0
+    
+  
+  
+  #return headlines
+  
+  
+  
+    
+
